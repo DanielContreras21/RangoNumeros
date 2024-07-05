979b799cbb9f23084bc85e0be49154df280bcf5f
Dcopito <danielcp62.dc@gmail.com>
2024-07-04 22:06:51 -0500

    Primer Push

---
+++ main.py
@@ -0,0 +1,10 @@
+valor = int(input("Ingresa el valor: "))
+minimo = 0
+maximo = 15
+
+if(valor>=minimo and valor <=maximo):
+    print("El valor está dentro del rango")
+else:
+    print("El valor está fuera del rango")
+
+
